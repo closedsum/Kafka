863d776419f805710255ff6bf39ee31da883d055
closedsum <closedsum@gmail.com>
2023-08-24 15:34:59 -0700

    updated git ignore

--- Content/Scripts/Cs/custom_typings/Cs.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="../../typings/ue.d.ts">/>
-// Library
-/// <reference path="../Library/Library_Common.ts">/>
-/// <reference path="../Library/Library_Function.ts">/>
-/// <reference path="../Library/Library_Array.ts">/>
